f5b411956e2a359f78eeaef6330667d5c86e3f2a
DavidLynch26 <lynchda78@gmail.com>
2022-11-28 14:56:19 -0500

    User login added for php/sql, js needed to collect form data

--- adminChecker.php
+++ adminChecker.php
@@ -5,10 +5,12 @@
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     if(count($results) < 1){
+        $hashedPass = password_hash("password123", PASSWORD_DEFAULT);
+
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conn->exec("ALTER TABLE users AUTO_INCREMENT = 1");
         $admin = "INSERT INTO users (firstname, lastname, password, email, role, created_at)
-                    VALUES ('admin', 'admin', 'Group1', 'admin@project2.com', 'ADMIN', NOW())";
+                    VALUES ('admin', 'admin', '$hashedPass', 'admin@project2.com', 'ADMIN', NOW())";
         $conn->exec($admin);
     }
     $conn = null;
--- verifyUser.php
+++ verifyUser.php
@@ -1,21 +1,24 @@
 <?php 
     include "setConnection.php";
 
-    if(isset($_POST['username']) and isset($_POST['password'])){
-        $userName = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+    if(isset($_POST['username']) and isset($_POST['password'])):
+        $login = false;
+        $userName = filter_var($_POST['username'], FILTER_SANITIZE_EMAIL);
         $passWord = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
 
         $queryString = "SELECT * FROM users";
 
         $stmt = $conn->query($queryString);
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach($results as $result){
-            if($result['email'] == $userName and password_verify($result['password'], $passWord)){
-                echo true;
-            }
-        }
-    }
-
-    $conn = null;
-?>
+        foreach($results as $result):
+            if($result['email'] == $userName and password_verify($passWord, $result['password'])): 
+                $login = true;?>
+                <p>Successfull Login</p>
+                <?php break;
+            endif; 
+        endforeach; 
+        if(!$login): ?>
+            <p>Unsuccessful Login</p>
+        <?php endif;
+    endif;
+    $conn = null;?>
