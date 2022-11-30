b727c570f3ff05ac5d67535c45ca5c576b3e13fe
kstar234 <32802115+kstar234@users.noreply.github.com>
2022-11-29 19:29:50 -0500

    Added basic login form(no php functionality yet)

---
+++ UserLogin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+     <form action="UserLogin.php" method="post">
+        <h2>LOGIN</h2>
+        <label>User Name</label>
+        <input type="text" name="uname" placeholder="User Name"><br>
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password"><br> 
+        <button type="submit">Login</button>
+     </form>
+</body>
+</html>
