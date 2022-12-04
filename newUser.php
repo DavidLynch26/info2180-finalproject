92d48f660b8e7aad6d5aee880fa71cf17645f996
DavidLynch26 <lynchda78@gmail.com>
2022-12-03 21:47:47 -0500

    new user file added, testing required

---
+++ newUser.php
@@ -0,0 +1,22 @@
+<h1>New User</h1>
+<form action="newUser.php">
+    <label for="firtsName"> First Name
+        <input type="text">
+    </label>
+    <label for="lastName"> Last Name
+        <input type="text">
+    </label>
+    <label for="email"> Email
+        <input type="text">
+    </label>
+    <label for="password"> Password
+        <input type="text">
+    </label>
+    <label for="roles"> Role
+        <select name="roles" id="roleSelector">
+            <option value="member">Member</option>
+            <option value="admin">Admin</option>
+        </select>
+    </label>
+    <button id = "saveButton" type = "button">Save</button>
+</form>
