7146775969010ff33d69c3f9b79bc55857393d2f
Ameliapell <114243723+Ameliapell@users.noreply.github.com>
2022-12-04 15:52:47 -0800

    Update AddNote.php

--- AddNote.php
+++ AddNote.php
@@ -2,23 +2,35 @@
 <form action="addnotes.php">
     <label for="notes"> notes
     </label>
-<h2> Jane Doe </h2>
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Numquam sapiente officiis suscipit assumenda presentium,</br><br> porro ad magni at distinctio vel beatae sit fugiat quia placeat necessitatibus,</br>
-    magnam provident veritatis perspiciatis?
-    November 11, 2022 at 10am</p>
-
-
-    <h2> Jane Doe </h2>
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Numquam sapiente officiis suscipit assumenda presentium,</br><br> porro ad magni at distinctio vel beatae sit fugiat quia placeat necessitatibus,</br>
-    magnam provident veritatis perspiciatis?
-    November 11, 2022 at 10am</p>
-
-    <h2> Jane Doe </h2>
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Numquam sapiente officiis suscipit assumenda presentium,</br><br> porro ad magni at distinctio vel beatae sit fugiat quia placeat necessitatibus,</br>
-    magnam provident veritatis perspiciatis?
-    November 11, 2022 at 10am</p>
-
-    <label for="addnote"> Add Note
-        <input type="text">
+    <h2> notes</h2>
+<form action="addnotes.php">
+    <label for="notes"> notes
+    </label><h2> Notes</h2>
+<form action="addnotes.php">
+    <label for="notes"> 
     </label>
-    <button id = "addButton" type = "button">add</button>
+<h2> Jane Doe </h2>
+        <input type="tel"
+            placeholder="Enter your content" />
+            <button>Update</button>
+        <br />
+        <br />
+    
+<h2> Jane Doe </h2>
+        <input type="tel"
+            placeholder="Enter your content" />
+            <button>Update</button>
+        <br />
+        <br />
+
+<h2> Jane Doe </h2>
+        <input type="tel"
+            placeholder="Enter your content" />
+            <button>Update</button>
+        <br />
+        <br />
+        <button>Save</button>
+    </form>
+</body>
+
+</html>
