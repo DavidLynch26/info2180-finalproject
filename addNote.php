317973cb7c8ebbae5038a4edf0be2164b6d25aa7
Alxlex <salmon.alex235@gmail.com>
2022-12-04 21:21:42 -0500

    Implementation of Add Notes

--- addNote.php
+++ addNote.php
@@ -1,36 +1,4 @@
-<h2> notes</h2>
-<form action="addnotes.php">
-    <label for="notes"> notes
-    </label>
-    <h2> notes</h2>
-<form action="addnotes.php">
-    <label for="notes"> notes
-    </label><h2> Notes</h2>
-<form action="addnotes.php">
-    <label for="notes"> 
-    </label>
-<h2> Jane Doe </h2>
-        <input type="tel"
-            placeholder="Enter your content" />
-            <button>Update</button>
-        <br />
-        <br />
-    
-<h2> Jane Doe </h2>
-        <input type="tel"
-            placeholder="Enter your content" />
-            <button>Update</button>
-        <br />
-        <br />
-
-<h2> Jane Doe </h2>
-        <input type="tel"
-            placeholder="Enter your content" />
-            <button>Update</button>
-        <br />
-        <br />
-        <button>Save</button>
-    </form>
-</body>
-
-</html>
+<label for="addnote"> Add Note
+    <input type="text" placeholder="Enter Note Here">
+</label>
+<button id = "addButton" type = "button">add</button>
