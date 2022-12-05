584d7153d8b5c7145bd0a24ee6074a868764d868
Alxlex <salmon.alex235@gmail.com>
2022-12-04 21:25:56 -0500

    Implemention of Add Notes

--- addNote.php
+++ addNote.php
@@ -1,4 +1,4 @@
-<label for="addnote"> Add Note
+<label for="addnote"> Add a note about
     <input type="text" placeholder="Enter Note Here">
 </label>
-<button id = "addButton" type = "button">add</button>
+<button id = "addButton" type = "button">Add Note</button>
