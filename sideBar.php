a1cda180706f4533667c37137aef686e10438a75
Alxlex <salmon.alex235@gmail.com>
2022-12-07 00:53:27 -0500

    Adjustment of sidebar and css file

--- sideBar.php
+++ sideBar.php
@@ -1,7 +1,9 @@
 <section id  = "sideBar">
-    <a href=""><img src="houseIcon.png" alt="Symbol for home">Home</a>
-    <a href=""><img src="newContactIcon.png" alt="Symbol for new contact">New Contact</a>
-    <a href=""><img src="userIcon.png" alt="Symbol for user">Users</a>
-    <hr><hr>
-    <a href=""><img src="logoutIcon.png" alt="Symbol for logput">Logout</a>
+    <ul>
+        <li><a href=""><img src="houseIcon.png" alt="Symbol for home">Home</a></li>
+        <li><a href=""><img src="newContactIcon.png" alt="Symbol for new contact">New Contact</a></li>
+        <li><a href=""><img src="userIcon.png" alt="Symbol for user">Users</a></li>
+    <hr>
+        <li><a href=""><img src="logoutIcon.png" alt="Symbol for logput">Logout</a></li>
+    </ul>
 </section>
