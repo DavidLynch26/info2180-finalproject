9ff5efffd62193d3e3014f5fd40100ef47fbd112
DavidLynch26 <lynchda78@gmail.com>
2022-12-06 19:37:42 -0500

    New contact branch refactored, csfr checks implemented

--- userList.php
+++ userList.php
@@ -1,29 +1,31 @@
 <?php include "sideBar.php"; ?>
 
-<h1>Users</h1>
-<button id = "newUserButton" type = "button">&#43; Add User</button>
-<table>
-      <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Role</th>
-        <th>Created</th>
-      </tr>
-<?php
-  include 'setConnection.php';
-    
-  $stmt= $conn->query("SELECT * FROM users");
-  $results= $stmt->fetchAll(PDO::FETCH_ASSOC);
- 
-?>
-  <?php foreach($results as $row): ?>
-      <tr>
-        <td></td>
-        <?php $name = $row["title"] . " ".$row["firstname"] ." ". $row["lastname"]?>
-        <td> <?= $name; ?></td>
-        <td> <?= $row["email"]; ?></td>
-        <td> <?= $row["role"]; ?></td>
-        <td> <?= $row["created_at"]; ?></td>
-      </tr>
-  <?php endforeach ?>
-</table>
+<section id = "loader">
+  <h1>Users</h1>
+  <button id = "newUserButton" type = "button">&#43; Add User</button>
+  <table>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          <th>Created</th>
+        </tr>
+  <?php
+    include 'setConnection.php';
+      
+    $stmt= $conn->query("SELECT * FROM users");
+    $results= $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
+  ?>
+    <?php foreach($results as $row): ?>
+        <tr>
+          <td></td>
+          <?php $name = $row["title"] . " ".$row["firstname"] ." ". $row["lastname"]?>
+          <td> <?= $name; ?></td>
+          <td> <?= $row["email"]; ?></td>
+          <td> <?= $row["role"]; ?></td>
+          <td> <?= $row["created_at"]; ?></td>
+        </tr>
+    <?php endforeach ?>
+  </table>
+</section>
--- userLoginForm.php
+++ userLoginForm.php
@@ -1,7 +1,12 @@
-<?php include "generateSession.php";?>
+<?php 
+    session_start();
+    include "generateCsfr.php";
+?>
 
 <form id = "loginForm" action="verifyUser.php" method="POST">
-<h1>LOGIN</h1>
+    <h1>LOGIN</h1>
+
+    <input type="hidden" id= "csrfToken" value= "<?php echo $key; ?>" >
     <input type="text" id = "email" name="email" placeholder="Email address"><br>
     <input type="password" id = "password" name="password" placeholder="Password"><br>
     <button type = "button" id = "loginButton"><img src="lockIcon.png" alt="Black lock icon"> Login</button>
