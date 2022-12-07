f5e6182b5c19d4e0fdb06f5e150c1237d257a0cc
DavidLynch26 <lynchda78@gmail.com>
2022-12-06 19:59:51 -0500

    removed redundant user login file

--- userLogin.php
+++
@@ -1,8 +0,0 @@
-<h2>LOGIN</h2>
-<form id = "loginForm "action="verifyUser.php" method="POST">
-    <label>User Name</label>
-    <input type="text" id = "username" name="username" placeholder="User Name" required><br>
-    <label>Password</label>
-    <input type="password" id = "password" name="password" placeholder="Password" required><br> 
-    <button type = "button" id = "loginButton">Login</button>
-</form>
